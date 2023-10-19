4971efa08344c03e66a608badfd603f279599047
crummy <crummynz@gmail.com>
2023-10-19 21:04:04 +1300

    Add logging

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,6 +2,7 @@
  * @link https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
  */
 export async function register() {
+    console.log("!!!!!!REGISTER", process.env.NEXT_RUNTIME)
     const isServer = process.env.NEXT_RUNTIME === "nodejs";
     if (isServer) {
         const os = await import("os");
